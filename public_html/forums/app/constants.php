63389fcfda3fbaa2652bfa247b0be6fbb6632591
GrizzlyEntertainment <47308540+GrizzlyEntertainment@users.noreply.github.com>
2022-02-16 03:19:01 -0600

    derp

--- public_html/forums/app/constants.php
+++ public_html/forums/app/constants.php
@@ -3,7 +3,7 @@
 define('MYSQL_HOST', 'grizzlyent.cpde7dtfjvvy.us-west-2.rds.amazonaws.com'); # usually localhost
 define('MYSQL_DATABASE', 'grizzly');
 define('MYSQL_USERNAME', 'admin');
-define('MYSQL_PASSWORD', '!003786dc');
+define('MYSQL_PASSWORD', 'tootlesyoufuckinncuck');
 
 define("dark_mode", 1);
 
